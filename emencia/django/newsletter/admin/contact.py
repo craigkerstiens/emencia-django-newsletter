366eeea5089184818771ddc2ae7925bbedaabe49
Craig Kerstiens <craig.kerstiens@gmail.com>
2011-05-23 17:16:57 -0700

    adding account levels and missing icon

--- emencia/django/newsletter/admin/contact.py
+++ emencia/django/newsletter/admin/contact.py
@@ -26,11 +26,11 @@ contacts_imported = Signal(providing_args=['source', 'type'])
 
 class ContactAdmin(admin.ModelAdmin):
     date_hierarchy = 'creation_date'
-    list_display = ('email', 'first_name', 'last_name', 'apps', 'tags', 'tester', 'subscriber',
+    list_display = ('email', 'name', 'apps', 'tags', 'tester', 'subscriber',
                     'valid', 'total_subscriptions', 'creation_date', 'related_object_admin')
     list_filter = ('subscriber', 'valid', 'tester', 'creation_date', 'modification_date')
-    search_fields = ('email', 'first_name', 'last_name', 'apps', 'tags')
-    fieldsets = ((None, {'fields': ('email', 'first_name', 'last_name', 'apps')}),
+    search_fields = ('email', 'name''apps', 'tags')
+    fieldsets = ((None, {'fields': ('email', 'name', 'apps')}),
                  (None, {'fields': ('tags',)}),
                  (_('Status'), {'fields': ('subscriber', 'valid', 'tester')}),
                  (_('Advanced'), {'fields': ('object_id', 'content_type'),
